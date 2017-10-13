4edccc19c83b1f92357a708653183d12a581d444
Nathan Kinkade <nath@nkinka.de>
2017-10-13 16:24:29 -0600

    Adds back the '-args -log.level warn' flag to query_tester, since pinning promtool in previous PR likely fixes the earlier breakage.

--- cmd/query_tester/main.go
+++ cmd/query_tester/main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("To run query tests:")
-	fmt.Println("go test -v github.com/m-lab/prometheus-support/cmd/query_tester")
+	fmt.Println("go test -v github.com/m-lab/prometheus-support/cmd/query_tester -args -log.level warn")
 }
