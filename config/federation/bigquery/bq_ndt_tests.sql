bf3941f7dc61dd9e113a36a27e49e894cd9b25ea
Stephen Soltesz <soltesz@google.com>
2017-11-06 13:43:01 -0500

    Add comments and headers

--- config/federation/bigquery/bq_ndt_tests.sql
+++ config/federation/bigquery/bq_ndt_tests.sql
@@ -1,3 +1,7 @@
+-- bq_ndt_tests counts the number of NDT tests within a REFRESH_RATE_SEC
+-- interval 36 hours earlier than the time of query. The 36 hour offset
+-- maximizes the number of NDT tests processed by the pipeline for that period.
+#standardSQL
 SELECT
   CASE connection_spec.data_direction
     WHEN 0 THEN "c2s"
@@ -20,6 +24,7 @@ FROM
 
 WHERE
     -- Count all tests with in a REFRESH_RATE_SEC interval 36 hours ago.
+    -- REFRESH_RATE_SEC is a template value replaced by the bigquery exporter at run time.
     UNIX_SECONDS(log_time) > CAST(UNIX_SECONDS(CURRENT_TIMESTAMP()) / (REFRESH_RATE_SEC) AS INT64) * (REFRESH_RATE_SEC) - (36 * 60 * 60)
 AND UNIX_SECONDS(log_time) < CAST(UNIX_SECONDS(CURRENT_TIMESTAMP()) / (REFRESH_RATE_SEC) AS INT64) * (REFRESH_RATE_SEC) - (36 * 60 * 60) + (REFRESH_RATE_SEC)
 
--- config/federation/bigquery/ndt_test_count_total.sql
+++ config/federation/bigquery/ndt_test_count_total.sql
@@ -1,7 +1,12 @@
-SELECT                                                                          
-    828544145 + count(test_id) as value                                         
-FROM                                                                            
+-- ndt_test_count_total counts the total number of NDT test (up & down) over the
+-- history of the project. The constant was taken from the legacy tables.
+#standardSQL
+SELECT
+    828544145 + count(test_id) as value
+FROM
     `measurement-lab.public.ndt`
-WHERE                                                                           
-    connection_spec.client_ip != '45.56.98.222'                                 
-AND log_time > TIMESTAMP("2017-05-01")
+WHERE
+    -- Ignore tests from eb.measurementlab.net
+    connection_spec.client_ip != '45.56.98.222'
+AND
+    log_time > TIMESTAMP("2017-05-01")
