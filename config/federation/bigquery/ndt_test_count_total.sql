1b788579055a87f48d5417d230da50ea93983580
Stephen Soltesz <soltesz@google.com>
2017-11-07 14:47:03 -0500

    Remove duplicates from ndt_test_count_total

--- config/federation/bigquery/ndt_test_count_total.sql
+++ config/federation/bigquery/ndt_test_count_total.sql
@@ -1,12 +1,18 @@
 -- ndt_test_count_total counts the total number of NDT test (up & down) over the
 -- history of the project. The constant was taken from the legacy tables.
 #standardSQL
+-- Number of rows without duplicates
 SELECT
-    828544145 + count(test_id) as value
-FROM
-    `measurement-lab.public.ndt`
+  828544145 + count(test_id) as value
+FROM (
+  SELECT
+    test_id,
+    ROW_NUMBER() OVER (PARTITION BY test_id) row_number
+  FROM
+    -- Use ndt_fast, which excludes rows from EB
+    `measurement-lab.public.ndt_fast`
+  WHERE
+    partition_date >= DATE('2017-05-01')
+)
 WHERE
-    -- Ignore tests from eb.measurementlab.net
-    connection_spec.client_ip != '45.56.98.222'
-AND
-    log_time > TIMESTAMP("2017-05-01")
+  row_number = 1
