3e09ce31229cd63af1443f514d611aed7bddc86e
Stephen Soltesz <soltesz@google.com>
2017-11-07 13:38:00 -0500

    Remove legacy sql queries.

--- config/federation/bigquery/ndt_test_count_total2.sql
+++
@@ -1,8 +0,0 @@
--- TODO: Do not use Legacy SQL. Retire this script in favor of ndt_test_count_total.sql.
-SELECT
-    828544145 + count(test_id) as value
-FROM
-    [measurement-lab:public.ndt]
-WHERE
-    connection_spec.client_ip != '45.56.98.222'
-AND log_time > TIMESTAMP("2017-05-01")
