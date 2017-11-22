02dbe95583d3edbb2b2acaaea9ee6cb69c613371
Stephen Soltesz <soltesz@google.com>
2017-11-22 15:28:35 -0500

    Add 99th and 99.9th percentiles.

--- config/federation/bigquery/bq_ndt_server.sql
+++ config/federation/bigquery/bq_ndt_server.sql
@@ -1,5 +1,5 @@
 #standardSQL
--- bq_ndt_metrics calculates the daily median rtt as well as avg and median
+-- bq_ndt_metrics calculates the daily median rtt AS well AS avg and median
 -- upload and download rates per machine. These values can help recognize major
 -- performance regressions of servers or kernels.
 --
@@ -14,12 +14,18 @@
 
 SELECT
     machine,
+    APPROX_QUANTILES(IF(direction = "s2c", download, NULL), 2)[OFFSET(1)] AS value_download_median_rate,
+    APPROX_QUANTILES(IF(direction = "c2s", upload, NULL), 2)[OFFSET(1)] AS value_upload_median_rate,
+    APPROX_QUANTILES(IF(direction = "s2c", rtt, NULL), 2)[OFFSET(1)] AS value_download_median_rtt,
+    APPROX_QUANTILES(IF(direction = "c2s", rtt, NULL), 2)[OFFSET(1)] AS value_upload_median_rtt,
+    -- TODO: evaluate utility of avg data.
     AVG(IF(direction = "s2c", download, NULL)) AS value_download_avg_rate,
     AVG(IF(direction = "c2s", upload, NULL)) AS value_upload_avg_rate,
-    APPROX_QUANTILES(IF(direction = "s2c", download, NULL), 2)[OFFSET(1)] as value_download_median_rate,
-    APPROX_QUANTILES(IF(direction = "c2s", upload, NULL), 2)[OFFSET(1)] as value_upload_median_rate,
-    APPROX_QUANTILES(IF(direction = "s2c", rtt, NULL), 2)[OFFSET(1)] as value_download_median_rtt,
-    APPROX_QUANTILES(IF(direction = "c2s", rtt, NULL), 2)[OFFSET(1)] as value_upload_median_rtt,
+    -- TODO: evaluate utility of percentile data.
+    APPROX_QUANTILES(IF(direction = "s2c", download, NULL), 100)[OFFSET(99)] AS value_download_99th_rate,
+    APPROX_QUANTILES(IF(direction = "s2c", download, NULL), 1000)[OFFSET(999)] AS value_download_999th_rate,
+    APPROX_QUANTILES(IF(direction = "c2s", upload, NULL), 100)[OFFSET(99)] AS value_upload_99th_rate,
+    APPROX_QUANTILES(IF(direction = "c2s", upload, NULL), 1000)[OFFSET(999)] AS value_upload_99_9th_rate,
     COUNT(*) AS value_tests
 FROM (
     SELECT
@@ -31,20 +37,20 @@ FROM (
           WHEN 0 THEN "c2s"
           WHEN 1 THEN "s2c"
           ELSE "error"
-          END as direction,
+          END AS direction,
 
-        -- Download as bits-per-second
+        -- Download AS bits-per-second
         8 * 1000000 * (web100_log_entry.snap.HCThruOctetsAcked /
               (web100_log_entry.snap.SndLimTimeRwin +
                     web100_log_entry.snap.SndLimTimeCwnd +
                         web100_log_entry.snap.SndLimTimeSnd)) AS download,
 
-        -- Upload as bits-per-second
+        -- Upload AS bits-per-second
         8 * 1000000 * (web100_log_entry.snap.HCThruOctetsReceived /
-              web100_log_entry.snap.Duration) as upload,
+              web100_log_entry.snap.Duration) AS upload,
 
-        -- Average RTT as seconds
-        web100_log_entry.snap.SumRTT/web100_log_entry.snap.CountRTT/1000 as rtt
+        -- Average RTT AS seconds
+        web100_log_entry.snap.SumRTT/web100_log_entry.snap.CountRTT/1000 AS rtt
 
     FROM
        `measurement-lab.public.ndt`
@@ -54,7 +60,7 @@ FROM (
         -- wait 36hours. So, this query should be run either at the end of the
         -- following day, or more often (e.g. hourly) so future queries see all
         -- data.
-        _PARTITIONTIME = TIMESTAMP(DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY))
+        _PARTITIONTIME = TIMESTAMP_SUB(TIMESTAMP_TRUNC(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 12 HOUR), DAY), INTERVAL 24 HOUR)
 
     -- Basic test quality filters for safe division.
     AND web100_log_entry.snap.Duration > 0
