61410c7ff95f799d3266d39e04309a67179addf8
Josh King <jking@chambana.net>
2018-03-27 15:35:43 -0400

    Apply changes based on code-review.

--- apply-global-prometheus.sh
+++ apply-global-prometheus.sh
@@ -77,7 +77,6 @@ GF_CLIENT_SECRET_NAME=GF_AUTH_GOOGLE_CLIENT_SECRET_${PROJECT/-/_}
 GF_CLIENT_ID_NAME=GF_AUTH_GOOGLE_CLIENT_ID_${PROJECT/-/_}
 # TODO: kubectl v1.7  supports --from-env-file=
 kubectl create configmap grafana-env \
-    "--from-literal=domain=${GRAFANA_DOMAIN}" \
     "--from-literal=root_url=https://${GRAFANA_DOMAIN}" \
     "--from-literal=gf_auth_google_client_secret=${!GF_CLIENT_SECRET_NAME}" \
     "--from-literal=gf_auth_google_client_id=${!GF_CLIENT_ID_NAME}" \
