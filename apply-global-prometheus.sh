14201ff9cd1b02de2867b1f99867ad1ac708dcea
Josh King <jking@chambana.net>
2018-06-28 13:40:03 -0400

    Change config-map and environment variable names for configmap-reload.

--- apply-global-prometheus.sh
+++ apply-global-prometheus.sh
@@ -103,9 +103,9 @@ kubectl create secret generic prometheus-auth \
 AUTH="${!PROM_AUTH_USER}:${!PROM_AUTH_PASS}"
 export ALERTMANAGER_URL=https://$AUTH@alertmanager.${PROJECT}.measurementlab.net
 # Pass appropriate URL to configmap-reload
-export CONFIGMAP_RELOAD_URL=https://$AUTH@prometheus.${PROJECT}.measurementlab.net/-/reload
-kubectl create configmap configmap-reload \
-    "--from-literal=configmap_reload_url=${CONFIGMAP_RELOAD_URL}" \
+export PROM_RELOAD_URL=https://$AUTH@prometheus.${PROJECT}.measurementlab.net/-/reload
+kubectl create configmap configmap-reload-urls \
+    "--from-literal=prometheus_reload_url=${PROM_RELOAD_URL}" \
     --dry-run -o json | kubectl apply -f -
 set -x
 
