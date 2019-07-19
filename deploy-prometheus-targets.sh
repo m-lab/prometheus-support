114ccf43cf2af772f1cdea03c715f08aaf734871
nkinkade <nath@nkinka.de>
2019-07-19 17:20:06 -0600

    Generate and deploy non-scraper Prometheus targets (#502)
    * Generates and deploys Prometheus targets for non-legacy services. This steps is being removed from the legacy operator repo and into this one.
    
    * Makes mlabconfig.py executable so that it can be run.
    
    * Fixes a mispelled variable.
    
    * Fixes filename.

---
+++ deploy-prometheus-targets.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -x
+set -u
+
+CACHE_CONTROL="Cache-Control:private, max-age=0, no-transform"
+
+USAGE="Usage: $0 <project>"
+PROJECT=${1:?Please provide project name: $USAGE}
+
+# Root directory of this script.
+BASEDIR=${PWD}
+
+# Generate the configs.
+${BASEDIR}/generate-prometheus-targets.sh > /dev/null
+
+# Be sure that gcloud is PATH
+source "${HOME}/google-cloud-sdk/path.bash.inc"
+
+# Authenticate all operations using the given service account.
+if [[ -f /tmp/${PROJECT}.json ]] ; then
+  gcloud auth activate-service-account --key-file /tmp/${PROJECT}.json
+else
+  echo "Service account key not found at /tmp/${PROJECT}.json!!"
+  echo "Using default credentials."
+fi
+
+# Copy the configs to GCS.
+gsutil -h "$CACHE_CONTROL" cp -r \
+  ${BASEDIR}/gen/${PROJECT}/prometheus \
+  gs://operator-${PROJECT}
