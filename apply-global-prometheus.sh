a257b713fa0e63afdca3efcefea9260aa80ca4d0
Roberto D'Auria <roberto@measurementlab.net>
2019-08-05 14:21:50 +0200

    Username is always "reboot".

--- apply-global-prometheus.sh
+++ apply-global-prometheus.sh
@@ -57,7 +57,6 @@ export AUTH="${!PROM_AUTH_USER}:${!PROM_AUTH_PASS}"
 # Evaluate the Prometheus configuration template.
 sed -e 's|{{PROJECT}}|'${PROJECT}'|g' \
     -e 's|{{BBE_IPV6_PORT}}|'${!bbe_port}'|g' \
-    -e 's|{{REBOOTAPI_BASIC_AUTH_USER}}|'${!rebootapi_user}'|g' \
     -e 's|{{REBOOTAPI_BASIC_AUTH_PASS}}|'${!rebootapi_pass}'|g' \
     config/federation/prometheus/prometheus.yml.template > \
     config/federation/prometheus/prometheus.yml
