72c7c10587a8ecec0d1b1e25840b43a9ff90a052
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-03-11 15:53:20 -0500

    Add new query for recent daily test counts

---
+++ config/federation/bigquery/bq_daily_tests.sql
@@ -0,0 +1,20 @@
+WITH ndt7_date_counts AS (
+  SELECT date, COUNT(*) AS total_rows
+  FROM `measurement-lab.ndt.ndt7`
+  WHERE date > DATE_SUB(CURRENT_DATE(), INTERVAL 180 DAY)
+  GROUP BY date
+), ndt5_date_counts AS (
+  SELECT partition_date AS date, COUNT(*) AS total_rows
+  FROM `measurement-lab.ndt.ndt5`
+  WHERE partition_date > DATE_SUB(CURRENT_DATE(), INTERVAL 180 DAY)
+  GROUP BY date
+), total_date_counts AS (
+  SELECT date, n5.total_rows + n7.total_rows AS value_count
+  FROM ndt7_date_counts AS n7 JOIN ndt5_date_counts AS n5 USING(date)
+  ORDER BY date desc
+)
+
+SELECT
+  CAST(-ROW_NUMBER() OVER() AS STRING) AS le, date, value_count
+FROM
+  total_date_counts
