0d5b7d6d02815d8673e4243ef4cfad809117eb48
Roberto D'Auria <roberto@measurementlab.net>
2023-08-28 17:54:39 +0200

    Move everything to apply-cluster.sh

--- apply-cluster.sh
+++ apply-cluster.sh
@@ -26,6 +26,17 @@ kubectl create configmap prometheus-cluster-config \
     --from-file=config/cluster/prometheus \
     --dry-run="client" -o json | kubectl apply -f -
 
+# Construct the per-project CLIENT_ID, CLIENT_SECRET and COOKIE_SECRET for OAuth.
+export OAUTH_PROXY_CLIENT_ID=OAUTH_PROXY_CLIENT_ID_${PROJECT/-/_}
+export OAUTH_PROXY_CLIENT_SECRET=OAUTH_PROXY_CLIENT_SECRET_${PROJECT/-/_}
+export OAUTH_PROXY_COOKIE_SECRET=OAUTH_PROXY_COOKIE_SECRET_${PROJECT/-/_}
+
+# Replace template variables in oauth2-proxy.yml.
+sed -i -e 's|{{OAUTH_PROXY_CLIENT_ID}}|'${!OAUTH_PROXY_CLIENT_ID}'|g' \
+    -e 's|{{OAUTH_PROXY_CLIENT_SECRET}}|'${!OAUTH_PROXY_CLIENT_SECRET}'|g' \
+    -e 's|{{OAUTH_PROXY_COOKIE_SECRET}}|'${!OAUTH_PROXY_COOKIE_SECRET}'|g' \
+    k8s/data-pipeline/deployments/oauth2-proxy.yml
+
 # Check for per-project template variables.
 if [[ ! -f "k8s/${CLUSTER}/${PROJECT}.yml" ]] ; then
   echo "No template variables found for k8s/${CLUSTER}/${PROJECT}.yml"
