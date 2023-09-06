e6aa6a81c8d1b4d092943eae8e8356c221f71bdc
Roberto D'Auria <roberto@measurementlab.net>
2023-09-06 17:57:25 +0200

    Change federation-data-pipeline config to use the new domain name. (#1011)

--- apply-global-prometheus.sh
+++ apply-global-prometheus.sh
@@ -57,6 +57,8 @@ sed -e 's|{{PROJECT}}|'${PROJECT}'|g' \
     -e 's|{{REBOOTAPI_BASIC_AUTH_PASS}}|'${!REBOOTAPI_BASIC_AUTH_PASS}'|g' \
     -e 's|{{PLATFORM_PROM_AUTH_USER}}|'${!PLATFORM_PROM_AUTH_USER}'|g' \
     -e 's|{{PLATFORM_PROM_AUTH_PASS}}|'${!PLATFORM_PROM_AUTH_PASS}'|g' \
+    -e 's|{{PROM_AUTH_USER}}|'${!PROM_AUTH_USER}'|g' \
+    -e 's|{{PROM_AUTH_PASS}}|'${!PROM_AUTH_PASS}'|g' \
     config/federation/prometheus/prometheus.yml.template > \
     config/federation/prometheus/prometheus.yml
 
